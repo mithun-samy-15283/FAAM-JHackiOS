0d00eb004073d2c2013151ea67b14c2047df4bad
mithun-samy-15283 <132125440+mithun-samy-15283@users.noreply.github.com>
2024-08-06 11:57:29 +0530

    Added base files

---
+++ FAAM/FAAM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FAAM
+//
+//  Created by Mithun-15283 on 06/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
